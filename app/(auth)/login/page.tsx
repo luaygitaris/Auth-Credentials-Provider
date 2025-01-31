f40def84e6c594ecc0b740b1f571fafe9b4c59e0
Luay Bachtiar <luaygitaris@gmail.com>
2025-01-31 08:43:13 +0700

    menghilangkam message pada Login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,23 +1,12 @@
 import FormLogin from '@/components/auth/form-login';
 import { GithubButton, GoogleButton } from '@/components/social-button';
 
-const Login = ({ searchParams }: { searchParams?: { error?: string } }) => {
-	const params = searchParams?.error;
+const Login = () => {
 	return (
 		<div className='p-6 space-y-4'>
 			<h1 className='text-2xl font-bold text-gray-900'>
 				Sign In to Your Account
 			</h1>
-			{params === 'OAuthAccountNotLinked' ? (
-				<div
-					className='p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100'
-					role='alert'
-				>
-					<span className='font-medium'>
-						Account Already use by other provider.
-					</span>
-				</div>
-			) : null}
 			<FormLogin />
 			<div className='my-4 flex items-center before:flex-1 before:border-t before:border-gray-300 after:flex-1 after:border-t after:border-gray-300'>
 				<p className='mx-4 mb-0 text-center font-semibold text-gray-600'>Or</p>
